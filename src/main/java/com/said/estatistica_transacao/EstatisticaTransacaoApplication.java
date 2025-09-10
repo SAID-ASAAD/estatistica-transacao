a7f3a74a87a6cee64bbb97bf04233c0e50a23356
SaidAsaad <saidyousef201513@gmail.com>
2025-09-10 14:55:48 -0300

    Initial commit

---
+++ src/main/java/com/said/estatistica_transacao/EstatisticaTransacaoApplication.java
@@ -0,0 +1,13 @@
+package com.said.estatistica_transacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstatisticaTransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstatisticaTransacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/said/estatistica_transacao/EstatisticaTransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.said.estatistica_transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstatisticaTransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
