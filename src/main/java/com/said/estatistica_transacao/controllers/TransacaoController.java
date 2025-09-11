e45d317cc7ff8018e767dfb995748704bac4974a
SaidAsaad <saidyousef201513@gmail.com>
2025-09-11 12:26:53 -0300

    Método receberTransacao e logs

---
+++ src/main/java/com/said/estatistica_transacao/controllers/TransacaoController.java
@@ -0,0 +1,25 @@
+package com.said.estatistica_transacao.controllers;
+
+import com.said.estatistica_transacao.infrastructure.dtos.TransacaoDto;
+import com.said.estatistica_transacao.services.TransacaoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/transacao")
+public class TransacaoController {
+
+    private final TransacaoService transacaoService;
+    public TransacaoController(TransacaoService transacaoService){
+        this.transacaoService = transacaoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> receberTransacao(@RequestBody TransacaoDto transacaoDto){
+        transacaoService.receberTransacao(transacaoDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+}
